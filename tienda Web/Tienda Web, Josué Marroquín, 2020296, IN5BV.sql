f8ae9201aa9096b11c3cc2d37af42b36d0a8ada9
Josué Daniel Marroquín Hernández <88805561+jmarroquin202096@users.noreply.github.com>
2022-07-18 22:59:12 -0600

    Tienda Wev

---
+++ tienda Web/Tienda Web, Josué Marroquín, 2020296, IN5BV.sql
@@ -0,0 +1,70 @@
+/*Josué Daniel Marroquín Hernández, 2020296, IN5BV*/
+CREATE DATABASE TiendaWeb;
+
+USE TiendaWev;
+
+CREATE TABLE IF NOT EXISTS Genero (
+	IdGenero INT NOT NULL,
+    Descripcion VARCHAR(45),
+    PRIMARY KEY (IdGenero)
+);
+
+CREATE TABLE IF NOT EXISTS Clientes (
+    IdCliente INT NOT NULL AUTO_INCREMENT,
+    Nombre VARCHAR(45),
+    Apellidos VARCHAR(45),
+    Direccion VARCHAR(45),
+    Telefono INT NULL,
+    Genero_IdGenero INT NOT NULL,
+    PRIMARY KEY (IdCLiente),
+    CONSTRAINT fk_GeneroClientes 
+    FOREIGN KEY (Genero_IdGenero) REFERENCES Genero (IdGenero)
+);
+
+CREATE TABLE IF NOT EXISTS  MetodoPago(
+	IdMetodoPago INT NOT NULL,
+    Descripcion VARCHAR(45),
+    PRIMARY KEY (IdMetodoPago)
+    );
+    
+CREATE TABLE IF NOT EXISTS  TipoPedido(
+	IdTipoPedido INT NOT NULL,
+    Descripcion VARCHAR(45),
+    PRIMARY KEY (IdTipoPedido)
+);
+
+CREATE TABLE IF NOT EXISTS  Pedidos(
+	IdPedido INT NOT NULL AUTO_INCREMENT,
+	Fecha DATE,
+    TipoPedido_IdPedido INT NOT NULL,
+    MetodoPago_IdMetodoPago INT NOT NULL,
+    MontoTotal DECIMAL(10,2),
+    Clientes_IdCliente INT NOT NULL,
+    PRIMARY KEY (IdPedido),
+    CONSTRAINT fk_Pedidos_TipoPedido
+    FOREIGN KEY (TipoPedido_IdPedido) REFERENCES TipoPedido(IdPedido),
+    CONSTRAINT fk_Pedidos_MetodoPago
+    FOREIGN KEY (MetodoPago_IdPago) REFERENCES MetodoPago(IdPago),
+    CONSTRAINT fk_Pedidos_CLientes
+    FOREIGN KEY (Clientes_IdCliente) REFERENCES Clientes(IdCliente)
+);
+
+CREATE TABLE IF NOT EXISTS  Productos(
+	IdProducto INT NOT NULL AUTO_INCREMENT,
+    Nombre VARCHAR(45),
+    Descripcion VARCHAR(45),
+    PreciosinIVA DOUBLE,
+    Stock INT,
+    PRIMARY KEY (IdProducto)
+);
+
+CREATE TABLE IF NOT EXISTS  PedidosProductos(
+	Pedidos_IdPedido INT NOT NULL,
+    Productos_IdProducto INT NOT NULL,
+    Cantidad INT, 
+    PRIMARY KEY (Pedidos_IdPedido, Productos_IdProducto),
+    CONSTRAINT fk_Pedidos_has_Productos_Pedidos
+    FOREIGN KEY (Pedidos_IdPedido) REFERENCES Pedidos(IdPedido),
+    CONSTRAINT fk_Pedidos_has_Productos_Productos
+    FOREIGN KEY (Productos_IdProducto) REFERENCES Productos(IdProducto)
+    );
